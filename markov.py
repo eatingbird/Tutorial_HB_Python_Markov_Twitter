993acfaba6a1b189a41651beeb023470dd3ac2c0
Hackbright Student <no-reply@hackbrightacademy.com>
2017-03-01 18:08:14 -0800

    WIP- Twitter login credentials added

--- markov.py
+++ markov.py
@@ -2,40 +2,45 @@ import os
 import sys
 from random import choice
 import twitter
+api = twitter.Api(consumer_key= os.environ['TWITTER_CONSUMER_KEY'],
+                  consumer_secret=os.environ['TWITTER_ACCESS_TOKEN_SECRET'],
+                  access_token_key=os.environ['TWITTER_ACCESS_TOKEN_KEY'],
+                  access_token_secret=os.environ['TWITTER_CONSUMER_SECRET'])
+
+chains = {}  # dictionary for all the texts going through
+
+# print api.VerifyCredentials()
+# {"id": 16133, "location": "Philadelphia", "name": "bear"}
 
 
-def open_and_read_file(filenames):
-    """Given a list of files, open them, read the text, and return one long
-        string."""
 
-    body = ""
+def open_and_read_file(file_path):
+    """Takes file path as string; returns text as string.
+    Takes a string that is a file path, opens the file, and turns
+    the file's contents as one string of text.
+    """
 
-    for filename in filenames:
-        text_file = open(filename)
-        body = body + text_file.read()
-        text_file.close()
-
-    return body
+    open_file = open(file_path)
+    return open_file.read()
 
 
-def make_chains(text_string):
-    """Takes input text as string; returns dictionary of markov chains."""
-
-    chains = {}
+def make_chains(text_string, chains):
+    """Takes input text as string; returns _dictionary_ of markov chains.
+    A chain will be a key that consists of a tuple of (word1, word2)
+    and the value would be a list of the word(s) that follow those two
+    words in the input text.
+    For example:
+        >>> make_chains("hi there mary hi there juanita")
+        {('hi', 'there'): ['mary', 'juanita'], ('there', 'mary'): ['hi'], ('mary', 'hi': ['there']}
+    """
 
     words = text_string.split()
 
-    for i in range(len(words) - 2):
-        key = (words[i], words[i + 1])
-        value = words[i + 2]
-
-        if key not in chains:
-            chains[key] = []
-
-        chains[key].append(value)
-
-        # or we could replace the last three lines with:
-        #    chains.setdefault(key, []).append(value)
+    # Solution 2
+    for index in range(0, len(words)-2):
+        key = tuple([words[index], words[index+1]])
+        chains.setdefault(key, []).append(words[index+2])
+    chains.setdefault((words[-2], words[-1]), []).append(None)
 
     return chains
 
@@ -43,27 +48,47 @@ def make_chains(text_string):
 def make_text(chains):
     """Takes dictionary of markov chains; returns random text."""
 
-    key = choice(chains.keys())
-    words = [key[0], key[1]]
-    while key in chains:
-        # Keep looping until we have a key that isn't in the chains
-        # (which would mean it was the end of our original text)
-        #
-        # Note that for long texts (like a full book), this might mean
-        # it would run for a very long time.
+    output_text = ""
 
-        word = choice(chains[key])
-        words.append(word)
-        key = (key[1], word)
+    # made key list and then choise the first key random
+    keys = chains.keys()
+    new_key = choice(keys)
 
-    return " ".join(words)
+    # add the first tuple key to the output_text
+    output_text = output_text + new_key[0] + " " + new_key[1]
 
+    # from index 2 to end which is none, the code runs
+    while True:
+        random_value = choice(chains[new_key])
+        if random_value is None:
+            break
+        output_text += " %s" % random_value
+        new_key = tuple([new_key[1], random_value])
+
+    return output_text
+
+
+def combine_texts(argv):
+    """Reads in arbitrary number of texts files and adds them to our dictionary."""
+
+    # split the argv
+    texts_to_add = sys.argv[1:]
+
+    for text in texts_to_add:
+        input_text = open_and_read_file(text)
+        make_chains(input_text, chains)
 
 def tweet(chains):
     # Use Python os.environ to get at environmental variables
     # Note: you must run `source secrets.sh` before running this file
     # to make sure these environmental variables are set.
-    pass
+
+    combine_texts(sys.argv)
+    random_text = make_text(chains)
+    tweet_text = random_text[:(140-16)]+" #changeTheRatio"
+    status = api.PostUpdate(tweet_text)
+    print status.text
+
 
 # Get the filenames from the user through a command line prompt, ex:
 # python markov.py green-eggs.txt shakespeare.txt
